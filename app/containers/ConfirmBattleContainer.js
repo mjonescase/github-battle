3b9018988684d02a4afa31c710384b866a369110
Mike Jones <mjones@onshift.com>
2016-05-25 20:00:19 -0400

    axios, promises, github api: done except user data dosent appear

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
     contextTypes: {
@@ -19,8 +20,13 @@ var ConfirmBattleContainer = React.createClass({
     componentDidMount: function (){
 	var query = this.props.location.query;
 	console.log('componentDidMount');
-	//fetch info from github then update state
-	//console.log('QUERY', query);
+	githubHelpers.getPlayerInfo([query.playerOne, query.playerTwo])
+	.then(function (players) {
+	    this.setState({
+		isLoading: false,
+		playerInfo: [players[0], players[1]]
+	    })
+	}.bind(this))
     },
     componentWillReceiveProps: function () {
 	console.log('componentWillReceiveProps');
