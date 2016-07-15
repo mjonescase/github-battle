292070e96d6c39fed60955491d96d77764a913d0
Mike Jones <mjones@onshift.com>
2016-07-15 16:27:52 -0400

    finished private components video, have some issues

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -4,15 +4,12 @@ var styles = require('../styles');
 var Link = require('react-router').Link;
 var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
-
-function puke(obj) {
-    return <pre>{JSON.stringify(obj, null, ' ')}</pre>
-}
+var MainContainer = require('../containers/MainContainer');
 
 function ConfirmBattle (props) {
     return props.isLoading === true
         ? <p> LOADING! </p>
-	: <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+	: <MainContainer>
 	    <h1>Confirm Players</h1>
 	    <div className='col-sm-8 col-sm-offset-2'>
 	        <UserDetailsWrapper header="Player 1">
@@ -31,11 +28,11 @@ function ConfirmBattle (props) {
 	        <div className='col-sm-12' style={styles.space}>
 	            <Link to='/playerOne'>
 	                <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
-	                
+
 	            </Link>
                 </div>
 	    </div>
-	  </div>
+	  </MainContainer>
 }
 
 ConfirmBattle.propTypes = {
